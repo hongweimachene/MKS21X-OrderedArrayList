9ce97f2f04b921ea9d696cc213b1a1869873d1d1
hongweimachene <hchen00@stuy.edu>
2018-11-03 14:51:02 -0400

    orderedlist

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -8,15 +8,26 @@ public class OrderedArrayList <T extends Comparable<T>> extends NoNullArrayList<
     super(startingCapacity);
   }
 
-  public T set(int index, T value) {
-    super.remove(index);
-  }
-
-  public int sort() {
-
+  public int sort(T value) {
+    int index = 0;
+    for (int i = 0; i < size(); i++) {
+      if (get(i).compareTo(value) < 0) {
+        index++;
+      }
+    }
+    return index;
   }
 
   public boolean add(T value) {
-
+    super.add(sort(value), value);
+    return true;
   }
+
+  public void add(int index, T value) {
+    add(value);
+  }
+
+  //public T set(int index, T value) {
+
+  //}
 }
